1c7ca6615bdd203d67c7c6afaeb524dfff72e6e4
Franz Bewerunge <franz.bewerunge@tier.app>
2020-10-22 16:05:09 +0200

    Updates

---
+++ precache-manifest.b8fcf647135efbb093972fbfee40e26b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc69a69f4164884a4b1d874435f3c0a2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a8536b510a1df30160a6",
+    "url": "/monster-rolodex/static/css/main.7d7bcda4.chunk.css"
+  },
+  {
+    "revision": "f446d9444977f330bfbe",
+    "url": "/monster-rolodex/static/js/2.70969f69.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/monster-rolodex/static/js/2.70969f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8536b510a1df30160a6",
+    "url": "/monster-rolodex/static/js/main.b66791d4.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
